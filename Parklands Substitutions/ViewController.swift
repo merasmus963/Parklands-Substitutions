9dce7feebffc29da05dc573db6ffe69652bdb1a2
merasmus963 <merasmus@parklands.co.za>
2017-07-07 11:55:03 +0200

    First Build
    This is the first iteration.

---
+++ Parklands Substitutions/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Parklands Substitutions
+//
+//  Created by xcode on 2017/07/07.
+//  Copyright © 2017 Parklands College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("ewh")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
